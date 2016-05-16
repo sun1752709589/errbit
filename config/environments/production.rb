d8f848454922e9ae3f35699934750b6b0a0d56e0
sun <1752709589@qq.com>
2016-05-16 18:46:32 +0800

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   # config.active_record.dump_schema_after_migration = false
+
+  # care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
 end
